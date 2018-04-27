c1634e3aafcd423b8faa96974be43432be155bab
Dale Stevens <dale@twilightcoders.net>
2018-04-27 17:11:33 -0600

    Break Atom into distinct classes

--- lib/erle/elements/atom.rb
+++ lib/erle/elements/atom.rb
@@ -1,43 +1,37 @@
 module ERLE
 
   class Atom < Term
-    @enclosed_pattern = /[A-z0-9_@\-\.]*/
-    @unopened_pattern = /[a-z][A-z0-9_@]*/
-
-    @open = @close = /'/
-
-    # enclosure /'/
-    pattern /^'#{@enclosed_pattern.source}'/
-    pattern /^#{@unopened_pattern.source}/
-    # pattern /^[a-z][a-z0-9_@\-\.]*/ # TODO: Handle uppercase
-
-    INNER_PATTERN = /(#{@unopened_pattern}|#{@enclosed_pattern}|)/
 
     def to_ruby
       @output ||= @input
     end
 
-  protected
+  end
 
-    # An atom is a literal, a constant with name.
-    # An atom is to be enclosed in single quotes (')
-    # if it does not begin with a lower-case letter
-    # or if it contains other characters than
-    # alphanumeric characters, underscore (_), or @.
-    def self.parse(parser)
-      if (parser.scan(open))
-        parser.scan(@enclosed_pattern)
-        result = parser.matched
-        parser.scan(close)
-      elsif parser.scan(@unopened_pattern)
-        result = parser.matched.to_sym
-      else
-        parser.raise_unexpected_token
-      end
+  class OpenedAtom < Atom
+    @pattern = /[a-z][A-z0-9_@]*/
+    pattern /^#{@pattern.source}/
 
-      new(result)
+    def self.new *args
+      superclass.new *args
     end
 
+    def self.parse(parser)
+      super do |result|
+        new(result.to_sym)
+      end
+    end
+  end
+
+  class ClosedAtom < Atom
+    @pattern = /[A-z0-9_@\:\-\. ]*/
+    # pattern /^#{@pattern.source}/
+    pattern /[^']*/
+    enclosure /'/
+
+    def self.new *args
+      superclass.new *args
+    end
 
   end
 
